098a3ad0e782d0aa90a3def1b6e2779e267f437d
schnoddelbotz <jan@hacker.ch>
2022-06-26 22:41:53 +0200

    Circular One - Audiobook - Chapter 2/3 (en) #FreeAssange

---
+++ hoerspiel/en/02_yeah_why_not.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+test -z $VOICE && VOICE=fred
+
+echo 02 Yeah why not
+
+
+## outsource own 00_ALWAYS or so -- todo
+say -v victoria "Circular 1 - Drucksache 1"
+sleep 1
+say -v victoria "By Claudia Latour and Martin Sonneborn"
+sleep 1
+say -v victoria "ASSANGE"
+sleep 0.5
+say -v victoria "2nd edition, revised & updated. Now even more depressing. Smiley!"
+sleep 2
+say -v veena "Welcome to this free, horrible audio book version. Please enjoy! Free Assange! Note he is INNOCENT, although you probably did not get that news - only the fake allegations to jail him via lobbyists. Listen!"
+sleep 2
+##
+
+
+say -v victoria "Chapter Two: Yeah why not"
+sleep 3
+
+say -v $VOICE "On 27 and 28 October 2021, in London, we will attend a trial in which the REALLY NEGOTIABLE, which has NEVER only affected Julian Assange himself, but always ALL OF US, is not even TALKED ABOUT. The (since 1066) meticulously rehearsed rules of procedure want it that way. That here not the big picture ITSELF, but only its legally permissible WRITS are up for negotiation. With which not the great whole ITSELF, but only the PARTS of a society which are affected by THIS (s)he parts of a "reality" produced by legal means. "
+sleep 2
+say -v $VOICE "And so it actually happens that on a full two days of hearings at the highest British court, the London High Court, the MAIN QUESTION concentrated in the Assange case does not even arise. Which of course is still whether a supposedly democratic constitutional state can misuse a 100-year-old US (anti-)espionage law, in order to - assisted by tradition-saturated judiciary apparatuses of friendly states under the rule of law (in the old European world) in a thousand-year chase of unprecedented Australian publicist so thoroughly that the only thing that could that the example made of him alone - because of the CRIMINAL EFFECT it has had investigative journalism of a Fourth Force, which even Fourth Force that is even remotely worthy of the name will henceforth be permanently prevented. "
+sleep 2
+say -v $VOICE "The British district court in which Australian publicist Julian Assange after a Kafkaesque cascade of prosecutorial (and governmental) cascade of prosecutorial (and governmental) manoeuvres last year, the British District Court had question in January this year with a disturbing \"Yes, why? not?!\", in what was the biggest democratic SCANDAL in the history of democracy."
+sleep 2
+say -v $VOICE "The first instance proceedings were (somewhat surprisingly) conducted by a bland District Judge named Vanessa Barrister. (District Judge by the name of Vanessa Baraitser, at the time the most the most undistinguished figure in the British judiciary at the time. had to offer. Neither her professional background, nor her curriculum vitae, nor an overview of the cases she had heard were publicly known. of the cases she tried - not even a blurred photograph. The average owner of a rural of a rural carwash had left more evidence of her actual more evidence of her actual existence than Vanessa Baraitser Vanessa Baraitser, noted British ex-diplomat Craig Murray. Footnote 1: Craig Murray on Baraitser: \"Someone suggested to me she might be a hologram, but I don’t think so. Holograms have more empathy.\""
+sleep 2
+say -v $VOICE "A handful of small claims cases had probably been assigned to her, exciting enough for the traditionally excitable (British) local press, after all: Sexual sexual harassment by and with police officers, provincial politicians with a penchant for surreptitious mobile phone snaps among pre-crossing skirt wearers, that sort of thing. Stuff. In ten years of district court practice, Baraitser had heard a single extradition case: France’s request for the rendition of a businessman (Alexandre Djouhri) implicated in state corruption surrounding ex-president Nicolas Sarkozy. The The matter was clear, Baraitser had approved it."
+sleep 2
+say -v $VOICE "And now the Assange case."
+
+
+# Intermission 
+sleep 3
+say -v Anna "John Cleese meldet aus dem Regie-Raum: Er würde gerne ein altes Petersilienwurzel-Tee-Rezept seiner Mutter preis geben zu den Preisselbeeren. Ob sich das einrichten liesse, fragt er in gepflegtem Englisch - Ich, Anna, habe das soeben für Sie simultan übersetzt. Sie können mich in Örlikon, Zürich treffen. Ab 200 die Stunde erzähle ich ihnen alles über Frisörsalons, was sie noch nie wissen wollten. Kennen Sie übrigens den ausgezeichnten Film Le Marie de la Coiffeuse - Der Mann der Frisöse? Ich, Jan, hatte ihn ein paar malt auf Twitter geteilt. Er lief einst im ZDF Sommer-Kino, als man noch über Erotik reden durfte. Ach Scheisse, Mann! John?! Sie dürfen. Auch wenn die Zuhörer womöglich längst den Faden verloren haben. Es geht ums Petersilienwurzel-Tee-Rezept Ihrer, also seiner Mutter. Nicht gegen Alzheimer aber - hören Sie selbst!"
+sleep 1
+say -v fred "Thank you Anna. Much appreciated. Well. Let me cite Dr. Martens - in 2018 he stated ..."
+say -v fred "Petersilienwurzel-Tee: Wirkung, Anwendung & Zubereitung. Petersilienwurzel und der daraus bereitete Tee haben sich zur Durchspülung bei Erkrankungen der ableitenden Harnwege und zur Durchspülungstherapie zur Vorbeugung und Behandlung von Nierengrieß bewährt. Alles über Zubereitung und Anwendung. Sie brauchen: 2 g fein geschnittene Petersilienwurzel, ¼ Liter kochendes Wasser, Übergießen Sie die Wurzeln mit dem kochendem Wasser. Lassen Sie den Tee 10 bis 15 Minuten zugedeckt ziehen und seihen Sie den Satz ab. Man trinkt 2-3 Tassen pro Tag. Tagesdosis: Tee aus 6 g Petersilienkraut"
+say -v Anna "Thank you John"
+say -v fred "Thank you Anna"
+say -v Anna "Thank YOU John!"
+say -v fred "Thank YOU ANNA!"
+
+say -v anna "oh john" & 
+  say -v fred "oh anna"
+say -v anna "Lass mich rein lass mich raus"
+say -v fred "anna"
+sleep 1
+say -v fred "oh anna"
+sleep 1
+say -v fred "berta"
+sleep 1
+say -v fred "berts"
+sleep 1
+say -v fred "lass mich rein, lass mich raus -- hier endet die Geschichte mit der Maus. Raus!"
