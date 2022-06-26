cfb636295af470cb39bb75a1f27744e83873d60f
schnoddelbotz <jan@hacker.ch>
2022-06-26 23:12:02 +0200

    Work around apple bug reported in https://twitter.com/JanHacker9/status/1541165326083280901/photo/1 #FreeAssange #Impediment #Blocker #Solved

--- hoerspiel/en/PLAY_ALL.sh
+++ hoerspiel/en/PLAY_ALL.sh
@@ -1,7 +1,19 @@
-#!/bin/sh
+#!/bin/sh -x
 
 test -z $VOICE && VOICE=fred
 
+say() {
+	retval=1
+	while [ $retval != 0 ]; do
+		/usr/bin/say $@
+		retval=$?
+		if [ $retval -ne 0 ]; then
+			echo "Failed, will retry forever!"
+			sleep 1
+		fi
+	done
+}
+
 for script in ./??_*.sh; do
-	env VOICE=$VOICE $script 
+	source $script 
 done
